5d5da3275c5c4365a537df90935d8d3255bac13c
Анастасия <tasyaosipova736@gmail.com>
2023-04-03 19:05:22 +0300

    Дан массив строк, необходимо удалить из него все дубли, оставшиеся строки объединить в одну в порядке следования в массиве.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.*;
+import java.util.stream.*;
+
+public class Main{
+    public static String keepDistinct(String ...arr){
+        return Arrays.stream(arr)
+                .distinct()
+                .collect(Collectors.joining());
+    }
+    public static void main(String[] args){
+        System.out.println(keepDistinct("aa","pppp","sss","aaa","bbb","pppp","bbb"));
     }
 }
